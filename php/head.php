790c731d42a9f476f458387dc67f4f98c0e2be37
Chris Soueidan <chris@loogart.com>
2018-05-17 08:06:28 -0400

    removed no follow index robots

--- php/head.php
+++ php/head.php
@@ -3,7 +3,6 @@ echo '
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="robots" content="noindex, nofollow">
     
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./css/bootstrap.css">
