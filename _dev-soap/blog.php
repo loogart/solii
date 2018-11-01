b7fd8801f1a3920a6516df5284e94b97c584b724
Chris Soueidan <chris.soueidan@gmail.com>
2018-11-01 07:32:23 -0400

    created new dev branch
    - replicated dev branch of soap media
    - consolidated oroginal splash page to this repo

---
+++ _dev-soap/blog.php
@@ -0,0 +1,159 @@
+<!doctype html> 
+<html lang="en">
+
+<head>
+    <?php include './php/head.php';?>
+    <title>Solii | Blog</title>
+
+    <style type="text/css">
+        a.card-title {
+            text-decoration: none;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <?php include './php/navbar.php';?>
+
+    <main>
+        <div class="content-area video-section">
+            <div class="container p-md-5">
+                <div class="row">
+                    <div class="col-12 mx-auto">
+                        <h1 class="my-3"><img class="logo-h1" src="./img/logomark-solii-black.svg">Solii Digest</h1>
+                        <p>The latest Solii thoughts and articles.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mx-auto">
+                        
+                        <div class="card-deck mb-3">
+                            <div class="card">
+                                <a href="./design-luxury-home-with-natural-light"><img class="card-img-top" src="./img/blog-natural-light-home.jpg" alt="Living room with natural light"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./ultimate-guide-to-choosing-a-new-door">
+                                        <h5>Why Design a Luxury Home With Natural Light in Mind</h5>
+                                    </a>
+                                    <a class="card-text" href="./ultimate-guide-to-choosing-a-new-door">
+                                        <p>There’s a reason why flowers always grow to face the sun.</p>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <a href="./front-door-smart-lock-benefits"><img class="card-img-top" src="./img/blog-smart-locks.jpg" alt="Door with Smart Locks"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./signs-you-need-to-upgrade-your-front-door">
+                                        <h5>The Benefits of a Smart Lock for Your Luxury Home</h5>
+                                    </a>
+                                    <a class="card-text" href="./signs-you-need-to-upgrade-your-front-door">
+                                        <p>In the 16th century, moats and drawbridges were humankind’s response to security threats to the home.</p>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                        
+
+                        <div class="card-deck mb-3">
+                            <div class="card">
+                                <a href="./ultimate-guide-to-choosing-a-new-door"><img class="card-img-top" src="./img/blog-luxury-door-1.jpg" alt="House with nice front door"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./ultimate-guide-to-choosing-a-new-door">
+                                        <h5>Your Ultimate Guide to Choosing a New Door</h5>
+                                    </a>
+                                    <a class="card-text" href="./ultimate-guide-to-choosing-a-new-door">
+                                        <p>The front door is the starting point of the journey through your home—a focal point not to be overlooked. </p>
+                                    </a>
+                                    <!-- <p class="card-text"><small class="text-muted">July 22, 2018</small></p> -->
+                                </div>
+                            </div>
+                            <div class="card">
+                                <a href="./signs-you-need-to-upgrade-your-front-door"><img class="card-img-top" src="./img/blog-front-door-1.jpg" alt="House with nice front door"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./signs-you-need-to-upgrade-your-front-door">
+                                        <h5>5 Signs You Need to Upgrade Your Front Door</h5>
+                                    </a>
+                                    <a class="card-text" href="./signs-you-need-to-upgrade-your-front-door">
+                                        <p>Although it’s the focal point of every entrance and exit we make in our home, front door maintenance is often overlooked. </p>
+                                    </a>
+                                    <!-- <p class="card-text"><small class="text-muted">July 22, 2018</small></p> -->
+                                </div>
+                            </div>
+                            <div class="card">
+                                <a href="./window-cleaning-tips"><img class="card-img-top" src="./img/blog-clean-window-1.jpg" alt="Clean Window Blog Post Image"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./window-cleaning-tips">
+                                        <h5>Need to Know Cleaning Tips for Your Windows</h5>
+                                    </a>
+                                    <a class="card-text" href="./window-cleaning-tips">
+                                        <p>There’s nothing worse than looking out your window and having a gorgeous view eclipsed by spots, streaks, dust, and fingerprints. </p>
+                                    </a>
+                                    <!-- <p class="card-text"><small class="text-muted">July 10, 2018</small></p> -->
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="card-deck mb-3">
+
+                            <div class="card">
+                                <a href="./heritage-buildings-in-ottawa"><img class="card-img-top" src="./img/blog-heritage-1.jpg" alt="Heritage Building Canada"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./heritage-buildings-in-ottawa">
+                                        <h5>4 Heritage Buildings in Ottawa That Will make You Swoon</h5>
+                                    </a>
+                                    <a class="card-text" href="./heritage-buildings-in-ottawa">
+                                        <p>Move over Parliament Hill, Ottawa has plenty of architecturally beautiful buildings that deserve our attention. </p>
+                                    </a>
+                                    <!-- <p class="card-text"><small class="text-muted">July 4, 2018</small></p> -->
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <a href="./epic-front-doors"><img class="card-img-top" src="./img/blog-doors-1.jpg" alt="Card image cap" style="object-fit:cover;height:200px"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./epic-front-doors">
+                                        <h5>5 Epic Front Doors That Will Give You Major Home Envy</h5>
+                                    </a>
+                                    <a class="card-text" href="./epic-front-doors">
+                                        <p>First impressions are everything, right? The same counts with your home.</p>
+                                    </a>
+                                    <!-- <p class="card-text"><small class="text-muted">June 28, 2018</small></p> -->
+                                </div>
+                            </div>
+                            <div class="card">
+                                <a href="./selecting-custom-luxury-windows"><img class="card-img-top" src="./img/blog1.jpg" alt="Luxury home interior"></a>
+                                <div class="card-body">
+                                    <a class="card-title" href="./selecting-custom-luxury-windows">
+                                        <h5>5 Things to Consider When Selecting Custom Luxury Windows</h5>
+                                    </a>
+                                    <a class="card-text" href="./selecting-custom-luxury-windows">
+                                        <p>Whether you’re building a custom home from scratch, or looking to replace the windows in your current home, windows are a big investment.</p>
+                                    </a>
+                                    <!-- <p class="card-text"><small class="text-muted">June 26, 2018</small></p> -->
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!--<div class="col-12 text-center mx-auto my-3">
+                        <div class="btn-group" role="group" aria-label="pagination">
+                            <button type="button" class="btn btn-outline-dark"><i class="fa fa-arrow-left"></i> Newer</button>
+                            <button type="button" class="btn btn-outline-secondary">Older <i class="fa fa-arrow-right"></i></button>
+                        </div>
+                    </div>
+                    -->
+                </div>
+
+            </div>
+        </div>
+    </main>
+
+    <?php include './php/footer.php';?>
+
+</body>
+
+</html>
