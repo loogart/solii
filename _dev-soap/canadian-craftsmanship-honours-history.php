2f3271478bfe884d1d0d6e995afd132389275ada
Chris Soueidan <chris@loogart.com>
2019-02-25 09:54:27 -0500

    Update canadian-craftsmanship-honours-history.php

--- _dev-soap/canadian-craftsmanship-honours-history.php
+++ _dev-soap/canadian-craftsmanship-honours-history.php
@@ -39,7 +39,10 @@
 
                         <h3>Le Chateau Frontenac</h3>
 
-                        <img class="img-responsive" src="./img/blog-img/2019-02-24/1_interior.jpg" alt="Photo of the interior of the Chateau Frontenac">
+                        <figure>
+                            <img class="img-responsive shadow" src="./img/blog-img/2019-02-24/1_interior.jpg" alt="Photo of the interior of the Chateau Frontenac">
+                            <figcaption><i>Image source</i> <a href="https://www.fairmont.com/frontenac-quebec/media/photos/" target="_blank">The Fairmont, Le Château Frontenac</a></figcaption>
+                        </figure>
 
                         <p>Apparently, Le Chateau Frontenac is said to be <a href="https://www.quebecregion.com/en/quebec-city-and-area/chateau-frontenac/" target="_blank">the world’s most photographed hotel</a>. Any guesses why? Perhaps because this hotel was the prototype for the now iconic string of chateau-style railway hotels built by the Canadian Pacific Railway, and is the purest expression of Chateau design among them all. The Chateau Frontenac was designed by architect Bruce Price who was particularly inspired by the medieval chateaux of the Loire Valley in France. Telling signs of this influence can be found throughout this historic landmark. The massive scale and fortress-like silhouette, circular and polygonal towers, and the steeply pitched roof define the building’s heritage character. </p>
 
@@ -47,22 +50,30 @@
 
                         <p>Historic significance, antique structural details and a national landmark -Belisle’s custom architectural solid wood windows are a perfect pairing to pay homage to the Chateau Frontenac’s history while offering comforts of the 21st century. </p>
 
-                        <img class="img-responsive" src="./img/blog-img/2019-02-24/_30A0977.jpg" alt="Photo of the detail of an exterior door at the Chateau Frontenac">
+                        <img class="img-responsive shadow" src="./img/blog-img/2019-02-24/_30A0977.jpg" alt="Photo of the detail of an exterior door at the Chateau Frontenac">
 
                         <h3>Vieux Quebec</h3>
-                        <img class="img-responsive" src="./img/blog-img/2019-02-24/6_tower.png" alt="Photo of a Vieux Quebec tower">
-
+                        <figure>
+                            <img class="img-responsive shadow" src="./img/blog-img/2019-02-24/6_tower.png" alt="Photo of a Vieux Quebec tower">
+                            <figcaption><i>Image source</i> <a href="https://www.belislewindows.com/style-realisations/historic/?lang=en" target="_blank">Belisle, Architectural Windows and Doors</a></figcaption>
+                        </figure>
 
                         <p>Steps away from the Chateau Frontenac you’ll find winding cobblestone streets full of shops and restaurants -welcome to Old Quebec. As a UNESCO world heritage site, Old Quebec is alive with history, historic architecture and European charm.</p>
 
                         <p>Gracing the storefronts of several city-loved spots, find the impeccable craftsmanship of Belisle Architectural Windows and Doors. Can you spot it? The sleek yet timeless profile? The solid yet enchanting curve of an arched doorway? These artisanal touches are a product of Belisle’s commitment to honouring history. It isa delicate fusion of time honoured tradition and innovative technology. There’s no wonder the merchants and residents of Old Quebec entrusted their historic windows and doors to Belisle. </p>
 
-                        <img class="img-responsive" src="./img/blog-img/2019-02-24/3_building.png" alt="Photo of a Vieux Quebec building">
+                        <figure>
+                            <img class="img-responsive shadow" src="./img/blog-img/2019-02-24/3_building.png" alt="Photo of a Vieux Quebec building">
+                            <figcaption><i>Image source</i> <a href="https://www.belislewindows.com/style-realisations/historic/?lang=en" target="_blank">Belisle, Architectural Windows and Doors</a></figcaption>
+                        </figure>
 
-                        <img class="img-responsive" src="./img/blog-img/2019-02-24/2_davids-tea.png" alt="Photo of a Vieux Quebec David's Tea Building">
+                        <figure>
+                            <img class="img-responsive shadow" src="./img/blog-img/2019-02-24/2_davids-tea.png" alt="Photo of a Vieux Quebec David's Tea Building">
+                            <figcaption><i>Image source</i> <a href="https://www.belislewindows.com/style-realisations/historic/?lang=en" target="_blank">FBelisle, Architectural Windows and Doors</a></figcaption>
+                        </figure>
 
                         <p>Do you have a heritage home and are looking to update? Architectural windows and doors are a seamless way to incorporate 21st century comforts while respecting your home’s history. Feeling inspired to add your own slice of Canadian heritage and craftsmanship to your home?</p>
-                        
+
                         <p>We’d love to hear from you! <a href="./contact">Contact us or visit our showroom today</a></p>
 
                         <hr class="mt-5">
